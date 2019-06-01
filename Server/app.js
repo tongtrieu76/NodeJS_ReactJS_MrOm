a20717ba072526652c44e43254d423b733112490
unknown <tongtrieu304@gmail.com>
2019-06-01 19:31:39 +0700

    server api post login va checktoken

--- Server/app.js
+++ Server/app.js
@@ -21,6 +21,13 @@ var socketIO = require("socket.io");
 var db = require("./routes/db/connect");
 
 var app = express();
+
+/// BUG1: fix bug lỗi Access-Control-Allow-Origin khi client get api của server vì thiếu cors header
+var cors = require('cors');
+app.use(cors());
+/// end BUG1.
+
+
 app.set("port", port);
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -35,6 +42,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api", LocationRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Server/routes/db/connect.js
+++ Server/routes/db/connect.js
@@ -21,12 +21,12 @@ var SchemaLocations = new mongoose.Schema({
 		default: "no_id"
 	},
 	Location_X: {
-		type:  Number,
-		default: 0
+		type:  String,
+		default: "0"
 	},
 	Location_Y: {
-		type:  Number,
-		default: 0
+		type:  String,
+		default: "0"
 	},
 	Date: {
 		type: Date,
@@ -92,12 +92,86 @@ var account = new mongoose.model('account',SchemaAccount);
 
 //Information Drivers
 var SchemaIfDrivers = new mongoose.Schema({
-
+	AccountID: {
+		type: String,
+		default: "no_id"
+	},
+	Birthday: {
+		type: Date,
+		default: Date.now
+	},
+	IdentityCard: {
+		type: String,
+		default: "no_IdentityCard"
+	},
+	Address: {
+		type: String,
+		default: "no_Address"
+	},
+	Email: {
+		type: String,
+		default: "no_Email"
+	},
+	NumberPhone: {
+		type: String,
+		default: "no_NumberPhone"
+	},
+	CarNumber: {
+		type: String,
+		default: "no_CarNumber"
+	},
+	CarInformation: {
+		type: String,
+		default: "no_CarInformation"
+	},
+	CarLicense: {
+		type: String,
+		default: "no_CarLicense"
+	},
+	CarSpecials: {
+		type: String,
+		default: "no_CarSpecials"
+	},
+	DateSignup: {
+		type: Date,
+		default: Date.now
+	},
+	Rate: {
+		type: String,
+		default: 5
+	}
 })
 var drivers = new mongoose.model('informationdriver',SchemaIfDrivers);
 //Information Users
 var SchemaIfUsers = new mongoose.Schema({
-
+	AccountID: {
+		type: String,
+		default: "no_id"
+	},
+	Birthday: {
+		type: Date,
+		default: Date.now
+	},
+	IdentityCard: {
+		type: String,
+		default: "no_IdentityCard"
+	},
+	Address: {
+		type: String,
+		default: "no_Address"
+	},
+	Email: {
+		type: String,
+		default: "no_Email"
+	},
+	NumberPhone: {
+		type: String,
+		default: "no_NumberPhone"
+	},
+	Point: {
+		type: Number,
+		default: 0
+	}
 })
 var users = new mongoose.model('informationuser',SchemaIfUsers);
 //exports
